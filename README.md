7e2a06beddaae4ab591b3dca61459ed3f9d73890
Thomas McWork <thomas.mc.work@gmail.com>
2017-10-27 13:13:09 +0200

    init

---
+++ README.md
@@ -0,0 +1,62 @@
+# ansible-test-role
+
+A tool to run and verify a single ansible role in a temporary virtual machine to verify it's correctness.
+
+Default vagrant image is Debian 9 64 bit (`debian/jessie64`).
+
+### What is it doing exactly?
+
+1. Prepare and start a temporary vagrant machine
+2. Create a temporary ansible inventory based on the vagrant machine data
+3. Run `ansible-playbook` with a temporary playbook only containing the role passed to the script as a parameter
+4. tear down the vagrant machine and remove all temporary files
+
+## Requirements
+
+- [ansible](http://docs.ansible.com/ansible/latest/intro_installation.html)
+- [vagrant](https://www.vagrantup.com/downloads.html)
+- any [compatible virtual machine environment](https://www.vagrantup.com/docs/providers/), e.g.
+    * `virtualbox`
+    * `libvirt`
+
+## Setup
+
+    sudo curl -O "/usr/local/bin/ansible-test-role" "https://github.com/..."
+    sudo chmod +x "/usr/local/bin/ansible-test-role"
+
+You could also replace the target to your personal bin folder (`$HOME/bin`).
+
+## Usage
+
+Simply `cd` into your ansible folder and then run:
+
+    ansible-test-role <role-name> [ansible-playbook options]
+
+`ansible-playbook options`:
+: These can be [valid `ansible-playbook` options](http://docs.ansible.com/ansible/latest/ansible-playbook.html) which wil be passed through.
+
+### Allowed environment variables
+
+You can define several environment variables which are used by [vagrant](https://www.vagrantup.com/docs/other/environmental-variables.html) or [ansible](http://docs.ansible.com/ansible/latest/config.html) like these:
+
+- `ANSIBLE_ROLES_PATH`: Allows you to invoke test from any folder in the filesystem. Good for CI
+- `VAGRANT_DEFAULT_PROVIDER`: The latest version of vagrant (2.0 as of 2017-10-27) isn't able to work with virtualbox version ≥ 5.2 – so you can easily switch to e.g. `libvirt` herewith
+
+### Forbidden environment variables
+
+These are defined in the script and thus overridden:
+
+- `ANSIBLE_RETRY_FILES_ENABLED`
+- `ANSIBLE_INVENTORY`
+- `VAGRANT_CWD`
+
+## Roadmap
+
+- enable command completion
+- enabled definition of a custom vagrant image
+- check role existence before starting the vagrant machine
+- add system tests
+
+## Thanks to
+
+… @berlic for [this inspiration](https://stackoverflow.com/a/38419466/2854723)
---
+++ ansible-test-role.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+# Test an ansible role solely.
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+
+if [ $# -lt 1 ]; then
+  cat <<HELP
+Wrapper script for ansible-playbook to apply single role.
+
+Usage: $0 <role-name> [ansible-playbook options]
+
+Examples:
+  $0 my_role
+  $0 apache-webserver -i 'custom_host,' -vv --check
+HELP
+  exit
+fi
+
+role_name=$1
+shift
+
+echo "Trying to apply role '${role_name}' ..."
+
+# define the overridable ansible roles path
+if [ -z "${ANSIBLE_ROLES_PATH:+x}" ]; then
+    echo "var is unset"
+    export ANSIBLE_ROLES_PATH="$(pwd)/roles"
+fi
+
+# prepare vagrant environment
+export VAGRANT_CWD=$(mktemp -d)
+
+cat > "${VAGRANT_CWD}/Vagrantfile" <<END
+Vagrant.configure(2) do |config|
+  config.vm.box = "debian/jessie64"
+  config.vm.network "private_network", type: "dhcp"
+  config.vm.define "ansible-test-role"
+end
+END
+
+vagrant box update
+
+# start virtual machine
+vagrant up
+sc=$?
+if [ $sc -ne 0 ]; then
+    echo "failed to bring up virtual machine: ${sc}"
+    exit 1
+fi
+
+if ! vagrant ssh -c exit 2> /dev/null; then
+    echo "failed to get ssh ping to the machine (vagrant ssh)"
+    vagrant status
+    exit 2
+fi
+
+machine_ip=$(vagrant ssh-config | grep -i HostName | cut -d' ' -f4)
+machine_private_key=$(vagrant ssh-config | grep -i IdentityFile | cut -d' ' -f4)
+echo "machine has booted: ${machine_ip}"
+
+export ANSIBLE_RETRY_FILES_ENABLED="False"
+
+# create temporary ansible invnetory file
+export ANSIBLE_INVENTORY=$(mktemp)
+cat > "$ANSIBLE_INVENTORY" <<END
+[vagrant]
+$machine_ip ansible_user=vagrant ansible_ssh_private_key_file=${machine_private_key}
+END
+
+# execute the virtual playbook
+ansible-playbook --ssh-extra-args "-o UserKnownHostsFile=/dev/null" --ssh-extra-args "-o StrictHostKeyChecking=no" \
+  -i "$ANSIBLE_INVENTORY" "$@" /dev/stdin <<END
+---
+- hosts: ${machine_ip}
+  become: yes
+
+  roles:
+    - ${role_name}
+END
+
+ansible-playbook --ssh-extra-args "-o UserKnownHostsFile=/dev/null" --ssh-extra-args "-o StrictHostKeyChecking=no" \
+  -i "$ANSIBLE_INVENTORY" "$@" /dev/stdin <<END
+---
+- hosts: ${machine_ip}
+  become: yes
+
+  roles:
+    - ${role_name}
+END
+
+# clean up
+rm -f "$ANSIBLE_INVENTORY"
+
+vagrant destroy -f
+sc=$?
+if [ $sc -ne 0 ]; then
+    echo "failed to remove virtual machine: ${sc}"
+    echo "    $ vagrant destroy -f"
+    echo "    $ rm -rfv '$VAGRANT_CWD'"
+    exit 3
+fi
+
+rm -rf "$VAGRANT_CWD"
